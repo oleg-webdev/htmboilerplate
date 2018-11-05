345339540d28d39f539423f3016e90fc082af3dc
oleg-webdev <cibulnicoleg@gmail.com>
2018-11-05 10:29:13 +0200

    watcher

--- src/app/components/home/home.js
+++ src/app/components/home/home.js
@@ -1,14 +1,19 @@
 angular.module('myApp.home', [])
 	.controller('homeCtrl', homeCtrl);
 
-homeCtrl.$inject = ['$scope', 'UserFactory'];
-function homeCtrl($scope, UserFactory) {
+homeCtrl.$inject = ['$scope', 'UserFactory', 'UserService'];
+function homeCtrl($scope, UserFactory, UserService) {
 
 	$scope.welcomeText = 'Home Router!';
 	$scope.userName = UserFactory.name;
+	$scope.usr = UserService.User;
 
 	$scope.$watch(function(){return UserFactory.name}, function(value) {
 		$scope.userName = value;
+	});
+
+	$scope.$watch(function(){return UserService.User}, function(value) {
+		$scope.usr = value;
 	});
 
 }
